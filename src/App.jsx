dc57d38dfcaabb1d50afa172f4e753270a18c937
Kiranraj27 <kiranraj586@gmail.com>
2023-08-18 19:06:40 +0530

    Logo and asset added

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,23 @@ import {
 
 const App = () => {
   return (
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar />
-        <Hero />
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
-      </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
